78487b313d54f54474bc0e9c2df8b06077eaf65f
Corel <48659688+corelvilches@users.noreply.github.com>
2024-01-29 00:04:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Función que se ejecuta al hacer clic en el botón
+function realizarPedido() {
+    // Obtener el párrafo por su ID
+    var miParrafo = document.getElementById("parrafoMensaje");
+    // Modificar el contenido del párrafo con el mensaje indicado
+    miParrafo.innerHTML = "Gracias! Tu pedido llegará rápidamente.";
+}
